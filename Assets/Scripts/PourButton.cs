2f7af4c0f645a6010b6113a2936b99362367d5d5
Rami Bokhari <rbisthebest@hotmail.com>
2025-01-24 16:32:21 +0300

    Implement bubble showing feature

--- Assets/Scripts/PourButton.cs
+++ Assets/Scripts/PourButton.cs
@@ -1,17 +1,26 @@
-using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 public class PourButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler// These are the interfaces the OnPointerUp method requires.
 {
     public float pourSpeed = 0.001f;
+    public float bubbleSpeed = 0.1f;
+    public int bubblePercentage = 50;
+    
     private GameObject substance;
+    private GameObject bubbleMask;
+
     private bool isPouring = false;
+    private bool donePouring = false;
+    private float bubbleHight;
 
     void Start()
     {
         substance = GameObject.FindWithTag("Substance");
+        bubbleMask = GameObject.FindWithTag("BubbleMask");
+
+        StartCoroutine(BubbleCoroutine());
     }
 
     void Update()
@@ -21,6 +30,22 @@ public class PourButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler/
             var scale = substance.transform.localScale;
             scale.y += pourSpeed;
             substance.transform.localScale = scale;
+        }
+    }
+
+    private IEnumerator BubbleCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.01f);
+
+            var bubbleScale = bubbleMask.transform.localScale;
+
+            if (donePouring && bubbleScale.y< bubbleHight)
+            {
+                bubbleScale.y += bubbleSpeed;
+                bubbleMask.transform.localScale = bubbleScale;
+            }
         }
     }
 
@@ -34,5 +59,9 @@ public class PourButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler/
     public void OnPointerUp(PointerEventData eventData)
     {
         isPouring =false;
+        bubbleHight = substance.transform.localScale.y +
+            substance.transform.localScale.y * (bubblePercentage/100f);
+
+        donePouring = true;
     }
 }
